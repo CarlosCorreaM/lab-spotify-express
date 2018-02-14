a3918e6b472c5b7d43b78794f045bc70c6009418
Carlos Correa <carloscorrea.dev@gmail.com>
2018-02-14 17:01:02 -0600

    fixed

---
+++ solution-code/app.js
@@ -0,0 +1,87 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+require("dotenv").config();
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Remember to paste here your credentials
+var clientId = process.env.clientId,
+  clientSecret = process.env.clientSecret,
+  accessToken = '';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: "1c30624cba6742dcb792991caecae571",
+  clientSecret: "87cce8c8af25466386513ca93a1755bd"
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  });
+
+app.get('/', (req, res, next) => {
+  res.render('home');
+});
+
+app.get('/artists', (req, res, next) => {
+  spotifyApi.searchArtists(req.query.artist)
+    .then(function(data) {
+      res.render('artists', {
+        artists: data.body.artists.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    .then(function(data) {
+      res.render('albums', {
+        albums: data.body.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId)
+    .then(function(data) {
+      res.render('tracks', {
+        tracks: data.body.items
+      });
+    }, function(err) {
+      console.log('Something went wrong!', err);
+    });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, () => console.log('Mágia de Spotify en el puerto: 3000! d(^o^)b'));
+
+module.exports = app;
